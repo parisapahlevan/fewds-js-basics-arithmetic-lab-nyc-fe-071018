95dd86a2085067dde488ca31bb5854eca1107070
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-14 11:53:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let newID = oldID + 1000000000
 let ageIsValid = (Number.isInteger(currentAge) && currentAge % 1 === 0) ? true : false
 let randomNumber = Math.floor(Math.random() * 21);
 let randomInteger = Math.floor(randomNumber)
-let randomUserID = Math.min(1000000001, ) randomInteger 
+let randomUserID = Math.min(1000000001, 1000000020) 
 
 
 
